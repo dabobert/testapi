c5dd305eba4fb84432cd05cc04879e5f8aae6eda
Robert Jenkins <bobert@gmail.com>
2012-04-23 20:07:18 -0400

    - api now takes depth param

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,14 @@
 class ApiController < ApplicationController
 
   def show
+    if params[:depth].blank?
+      depth = 3
+    else
+      depth = params[:depth].to_i
+    end
+    
     @object = ArtistName.seek(params[:id]) || ReleaseName.seek(params[:id])
-    @response = @object.origin(3)
+    @response = @object.origin(depth)
     respond_to do |format|
       format.html # show.html.erb
       format.json  { render :json => response.to_json }
